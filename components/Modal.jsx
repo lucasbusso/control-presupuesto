5af51c0fc103dc7f6cbf9ffee418e9389316afe3
Lucas Busso <lucasxbusso@gmail.com>
2022-03-28 11:21:42 -0300

    instalando react circular progressbar

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import Mensaje from './Mensaje'
 import cerrarModal from '../src/img/cerrar.svg'
 
-const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto, gastoEditar}) => {
+const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto, gastoEditar, setGastoEditar}) => {
   const [mensaje, setMensaje] = useState('')
   const [nombre, setNombre] = useState('')
   const [cantidad, setCantidad] = useState('')
@@ -22,6 +22,7 @@ const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto, gastoEditar
 
   const ocultarModal = () => {
         setAnimarModal(false)
+        setGastoEditar({})
         
         setTimeout(() => {
               setModal(false)
--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,7 @@ function App() {
     if (gasto.id){
       const gastosActualizados = gastos.map( gastoState => gastoState.id === gasto.id ? gasto : gastoState)
       setGastos(gastosActualizados);
+      setGastoEditar({})
     } else {
       gasto.id = generarId(); 
       gasto.fecha = Date.now();
@@ -93,6 +94,7 @@ function App() {
                     setAnimarModal={setAnimarModal}
                     guardarGasto={guardarGasto}
                     gastoEditar={gastoEditar}
+                    setGastoEditar={setGastoEditar}
                     />}
    </div>
   )
